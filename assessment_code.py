59c929b1dbab86e2cecff8e5651a08e433a0215e
tangiwai <jab2508@arastudent.ac.nz>
2023-06-10 00:15:04 +1200

    Building on the app.run assessment_code.py file

--- assessment_code.py
+++ assessment_code.py
@@ -177,5 +177,5 @@ def api_page():
 
 # If the app was called from the command line then run the app on TCP port 5002, turning debug messages on
 if __name__ == '__main__':
-  app.run(host='127.0.0.1' ,port='5050',debug=True)
+  app.run(host='0.0.0.0' ,port='5050',debug=True)
 
